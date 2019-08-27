eb2c4855aca7217ad397eab6f967bf422335e021
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2019-08-28 04:41:48 +0530

    Fix demo for py3

--- example/demo.py
+++ example/demo.py
@@ -2,14 +2,15 @@
 import time
 import subprocess
 
-cmd = ['backdrop','-x', '200', '-y' '500', '-w', '60', '-h', '78']
+cmd = ['backdrop','-l', '2', '-x', '200', '-y' '500', '-w', '60', '-h', '78']
 
 backdrop_process = subprocess.Popen(cmd, stdin=subprocess.PIPE)
 
-time.sleep(10)
+time.sleep(4)
 
-backdrop_process.stdin.write("400,100,400,300\n")
+backdrop_process.stdin.write('400,100,400,300\n'.encode())
+backdrop_process.stdin.flush()
 
-time.sleep(10)
+time.sleep(4)
 
 backdrop_process.terminate()
